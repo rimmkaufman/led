fa3678baf3f1dbed4e1bb3e66b37683ffcb7a2bb
aprk <alan@rimmkaufman.com>
2008-11-20 09:38:10 -0500

    second

--- Screen.rb
+++ Screen.rb
@@ -1,20 +1,20 @@
 class Screen
-	
-	attr_accessor :background_color
-	
-	BLACK = [0,0,0]
-	RED = [255,0,0]
-		
+
+  attr_accessor :background_color
+
+  BLACK = [0,0,0]
+  RED = [255,0,0]
+
   def initialize(rows,cols,background_color=BLACK)
-    @screen = [ [false] * cols ] * rows 
+    @screen = [ [false] * cols ] * rows
     @background_color = background_color
   end
 
-	def
+  def
 
-	def blit(glyph, row, col, color=RED) 
-    @glyphs[c]
+    def blit(glyph, row, col, color=RED)
+      @glyphs[c]
+    end
+
+
   end
-
-
-end
